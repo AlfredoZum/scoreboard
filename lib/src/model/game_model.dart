791d617e4c79fcb426b7bca630148f62bb0a9065
alfredo.conomia <alfredo.conomia@gmail.com>
2020-05-24 00:24:03 -0500

    Create model for game and gamesPlayed

--- lib/src/model/game_model.dart
+++ lib/src/model/game_model.dart
@@ -1,29 +1,22 @@
-import 'dart:io';
-
-class PlayerModel {
+class GameModel {
 
   int id;
   String name;
-  String image;
-  File fileImage;
   String createAt;
   String updateAt;
   int status;
 
-  PlayerModel({
+  GameModel({
     this.id,
     this.name,
-    this.image,
-    this.fileImage,
     this.createAt,
     this.updateAt,
     this.status,
   });
 
-  factory PlayerModel.fromJson(Map<String, dynamic> json) => new PlayerModel(
+  factory GameModel.fromJson(Map<String, dynamic> json) => new GameModel(
     id   : int.tryParse( json["id"].toString() ),
-    name : json["name"].toString(),
-    image: json["image"].toString(),
+    name: json["name"].toString(),
     createAt: json["create_at"].toString(),
     updateAt: json["update_at"].toString(),
     status: int.tryParse( json["status"].toString() ),
@@ -32,7 +25,6 @@ class PlayerModel {
   Map<String, dynamic> toJson() => {
     "id"   : id,
     "name" : name,
-    "image": image,
     "create_at": createAt,
     "update_at": updateAt,
     "status": status,
--- lib/src/model/games_played_model.dart
+++ lib/src/model/games_played_model.dart
@@ -1,29 +1,22 @@
-import 'dart:io';
-
-class PlayerModel {
+class GamesPlayedModel {
 
   int id;
-  String name;
-  String image;
-  File fileImage;
+  int gamesId;
   String createAt;
   String updateAt;
   int status;
 
-  PlayerModel({
+  GamesPlayedModel({
     this.id,
-    this.name,
-    this.image,
-    this.fileImage,
+    this.gamesId,
     this.createAt,
     this.updateAt,
     this.status,
   });
 
-  factory PlayerModel.fromJson(Map<String, dynamic> json) => new PlayerModel(
+  factory GamesPlayedModel.fromJson(Map<String, dynamic> json) => new GamesPlayedModel(
     id   : int.tryParse( json["id"].toString() ),
-    name : json["name"].toString(),
-    image: json["image"].toString(),
+    gamesId: int.tryParse( json["gamesId"].toString() ),
     createAt: json["create_at"].toString(),
     updateAt: json["update_at"].toString(),
     status: int.tryParse( json["status"].toString() ),
@@ -31,8 +24,7 @@ class PlayerModel {
 
   Map<String, dynamic> toJson() => {
     "id"   : id,
-    "name" : name,
-    "image": image,
+    "gamesId" : gamesId,
     "create_at": createAt,
     "update_at": updateAt,
     "status": status,
